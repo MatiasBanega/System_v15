a5ba8946f6ba6512d3d47e31e50fea75f693f71c
Ricardo Figueroa <112577688+BecaOdoo@users.noreply.github.com>
2022-11-07 12:54:58 -0500

    Add files via upload

--- whatsapp_connector_send_sale/__manifest__.py
+++ whatsapp_connector_send_sale/__manifest__.py
@@ -10,28 +10,34 @@
 # afecten el envío de mensajes a través de Acruxlab o a evitar contratar un Plan con Acruxlab.
 # Apoya nuestro trabajo y permite que sigamos mejorando este módulo y el servicio!
 # =====================================================================================
+
 {
-    'name': 'ChatRoom PACK. Send from many places and Templates',
-    'summary': 'Pack of modules to Send WhatsApp message from many places with Templates (Sale, Invoice, Purchase, CRM Leads, Product, Stock Picking, Partner). apichat.io GupShup Chat-Api ChatApi. ChatRoom 2.0.',
-    'description': 'Send WhatsApp message from many places with Templates (Sale, Invoice, Purchase, CRM Leads, Product, Stock Picking, Partner). All in one screen. ChatRoom 2.0.',
-    'version': '0.2',
+    'name': 'ChatRoom SEND Sale Order and Templates. WhatsApp',
+    'summary': 'Send messages from Sale Order with Templates.',
+    'description': 'Send Sales Orders. Sale Templates. Real All in One. Send and receive messages. Real ChatRoom. WhatsApp integration. WhatsApp Connector. apichat.io. GupShup. Chat-Api. ChatApi. Drag and Drop.',
+    'version': '15.0.1',
     'author': 'AcruxLab',
     'live_test_url': 'https://chatroom.acruxlab.com/web/signup',
     'support': 'info@acruxlab.com',
     'price': 0.0,
     'currency': 'USD',
-    'images': ['static/description/Banner_PACK_v10.png'],
+    'images': ['static/description/Banner_PACK_v4.png'],
     'website': 'https://acruxlab.com/plans',
     'license': 'OPL-1',
-    'application': False,
+    'application': True,
     'installable': True,
-    'active': False,
+    'auto_install': True,
     'category': 'Discuss',
     'depends': [
-        'whatsapp_connector',
+        'whatsapp_connector_template_base',
+        'sale',
     ],
     'data': [
+        'data/data.xml',
+        'views/res_config_settings_views.xml',
+        'views/form_views.xml',
     ],
-    'qweb': [
-    ],
+    'assets': {
+    },
+    'pre_init_hook': 'pre_init_hook',
 }
